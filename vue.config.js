c4a7dc4b8b3ea28d5dcbec176cfaac87af5a5998
Jyuns <jyuns@Jyunsui-MacBookPro.local>
2020-07-11 20:14:44 +0900

    set icon

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        mac: {
+          icon: './src/assets/icon.ico'
+        },
+        win: {
+          icon: './src/assets/icon.ico'
+        }
+      }
+    }
+  }
 }
